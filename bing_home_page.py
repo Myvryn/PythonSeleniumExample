e2e82c249fae2c1615612784fd3592fe16f698ec
Myvryn <ejones@estevenjones.com>
2014-04-22 09:56:15 -0700

    initial checkin

---
+++ bing_home_page.py
@@ -0,0 +1,63 @@
+__author__ = 'EddyJones'
+
+url = {
+    'start_url': 'http://www.bing.com'
+}
+
+menu_bar = {
+    'link_images': 'id.scpt0',
+    'link_videos': 'id.scpt1',
+    'link_maps': 'id.scpt2',
+    'link_news': 'id.scpt3',
+    'link_search_history': 'id.scpt4',
+    'link_more': 'id.scpt5',
+    'link_msn': 'id.scpt6',
+    'link_outlook_dot_com': 'id.scpt7',
+    'link_make_bing_homepage': 'id.id_d'
+}
+
+main_page = {
+    'textbox_search': 'id.sb_form_q',
+    'button_go': 'id.sb_form_go'
+}
+
+
+def get_object_type(item):
+    """Returns the type of identifier used by the object."""
+    return item.split('.')[0]
+
+
+def get_object_name(item):
+    """Returns the name of the identifier used by the object"""
+    return item.split('.')[1]
+
+
+def enter_text_in_search_box(text, driver):
+    """Enters the passed text into the Bing Search Box"""
+    search_box = get_selenium_element(main_page['textbox_search'], driver)
+    search_box.clear()
+    search_box.send_keys(text)
+    return
+
+
+def click_the_go_button(driver):
+    """Clicks the search submit button"""
+    go_button = get_selenium_element(main_page['button_go'], driver)
+    go_button.click()
+    return
+
+
+def is_search_term_in_title(search_term, driver):
+    """I would normally put this in a bing_result_page 'class' but it fits here for this example"""
+    return search_term + " - Bing" == driver.title
+
+
+def get_selenium_element(element, driver):
+    """Returns the selenium element by parsing the dictionary's id"""
+    item_type = element.split('.')[0]
+    item_id = element.split('.')[1]
+    if item_type == 'id':
+        element = driver.find_element_by_id(item_id)
+    else:
+        element = driver.find_element_by_name(item_id)
+    return element
---
+++ bing_test.py
@@ -0,0 +1,26 @@
+__author__ = 'EddyJones'
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import bing_home_page
+
+driver = webdriver.Firefox()
+
+""" TEST:  Navigate to bing.com """
+url = bing_home_page.url['start_url']
+driver.get(url)
+assert "Bing" in driver.title
+
+""" TEST:  Type in a search term and verify that term is in the title bar """
+search_terms = ['selenium',
+                'esp guitars',
+                'classic rock',
+                'Mötley Crüe']
+for search_term in search_terms:
+    bing_home_page.enter_text_in_search_box(search_term, driver)
+    bing_home_page.click_the_go_button(driver)
+    assert bing_home_page.is_search_term_in_title(search_term, driver)
+
+driver.close()
+
+
